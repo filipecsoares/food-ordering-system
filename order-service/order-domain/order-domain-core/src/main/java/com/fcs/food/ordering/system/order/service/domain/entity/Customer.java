31c9c618be8ba461f752c730faf3ce8636fc36d5
filipecsoares <filipecarlossoares@gmail.com>
2022-09-17 15:25:21 -0300

    create order events

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/fcs/food/ordering/system/order/service/domain/entity/Customer.java
@@ -0,0 +1,7 @@
+package com.fcs.food.ordering.system.order.service.domain.entity;
+
+import com.fcs.food.ordering.system.domain.entity.AggregateRoot;
+import com.fcs.food.ordering.system.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
